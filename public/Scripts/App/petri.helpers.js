0066b77b7a672b1a1b99dad368036335f76dad22
PavlyukVadim <pavlyuk.dev@gmail.com>
2021-01-05 18:23:02 +0200

    code style for helpers

--- public/Scripts/App/petri.helpers.js
+++ public/Scripts/App/petri.helpers.js
@@ -40,33 +40,23 @@ function restorePlace(place, plainObject) {
 }
 
 function restoreTransition(transition, plainObject) {
-    if (plainObject.deviationParamName) {
-        transition.setDeviationParam(plainObject.deviationParamName);
-    }
-    if (plainObject.delayParamName) {
-        transition.setDelayParam(plainObject.delayParamName);
-    }
-    if (plainObject.channelsParamName) {
-        transition.setChannelsParam(plainObject.channelsParamName);
-    }
-    if (plainObject.distributionParamName) {
-        transition.setDistributionParam(plainObject.distributionParamName);
-    }
-    if (plainObject.priorityParamName) {
-        transition.setPriorityParam(plainObject.priorityParamName);
-    }
-    if (plainObject.probabilityParamName) {
-        transition.setProbabilityParam(plainObject.probabilityParamName);
-    }
+    plainObject.deviationParamName && transition.setDeviationParam(plainObject.deviationParamName);
+    plainObject.delayParamName && transition.setDelayParam(plainObject.delayParamName);
+    plainObject.channelsParamName && transition.setChannelsParam(plainObject.channelsParamName);
+    plainObject.distributionParamName && transition.setDistributionParam(plainObject.distributionParamName);
+    plainObject.priorityParamName && transition.setPriorityParam(plainObject.priorityParamName);
+    plainObject.probabilityParamName && transition.setProbabilityParam(plainObject.probabilityParamName);
     return transition;
 }
 
 function restoreArc(arc, plainObject) {
     arc.isOneOfTwo = plainObject.isOneOfTwo;
     arc.isFirst = plainObject.isFirst;
+
     if (plainObject.channelsParamName) {
         arc.setChannelsParam(plainObject.channelsParamName);
     }
+
     if (plainObject.isInformationLinkParamName) {
         arc.setIsInformationLinkParam(plainObject.isInformationLinkParamName);
     }
@@ -100,28 +90,33 @@ function restorePetriNet(net) {
 }
 
 function parsePetriNet(jsonNet) {
-    var simpleNetObject = JSON.parse(jsonNet, netParseCensor);
-    var petriNet = new PetriNet(simpleNetObject.name);
+    const simpleNetObject = JSON.parse(jsonNet, netParseCensor);
+    const petriNet = new PetriNet(simpleNetObject.name);
+
     petriNet.id = simpleNetObject.id;
     $.extend(petriNet, simpleNetObject);
-    var allPlaces = {};
-    var allTransitions = {};
-    for (var i = 0; i < petriNet.places.length; i++) {
-        var place = petriNet.places[i];
-        var petriPlace = new Place(place.id, place.name, place.markers, place.top, place.left);
+
+    const allPlaces = {};
+    const allTransitions = {};
+
+    for (let i = 0; i < petriNet.places.length; i++) {
+        const place = petriNet.places[i];
+        const petriPlace = new Place(place.id, place.name, place.markers, place.top, place.left);
         petriNet.places[i] = restorePlace(petriPlace, place);
         allPlaces[place.id] = petriNet.places[i];
     }
-    for (var i = 0; i < petriNet.transitions.length; i++) {
-        var tran = petriNet.transitions[i];
-        var pTransition = new Transition(tran.id, tran.name, tran.delay, tran.deviation, tran.distribution, tran.priority, tran.probability, tran.channels,
+
+    for (let i = 0; i < petriNet.transitions.length; i++) {
+        const tran = petriNet.transitions[i];
+        const pTransition = new Transition(tran.id, tran.name, tran.delay, tran.deviation, tran.distribution, tran.priority, tran.probability, tran.channels,
             tran.top, tran.left);
         petriNet.transitions[i] = restoreTransition(pTransition, tran);
         allTransitions[tran.id] = petriNet.transitions[i];
     }
-    for (var i = 0; i < petriNet.arcs.length; i++) {
-        var arc = petriNet.arcs[i];
-        var petriArc = new Arc(arc.id, allPlaces[arc.placeId], allTransitions[arc.transitionId], arc.fromPlace, arc.channels, arc.isInformationLink);
+
+    for (let i = 0; i < petriNet.arcs.length; i++) {
+        const arc = petriNet.arcs[i];
+        const petriArc = new Arc(arc.id, allPlaces[arc.placeId], allTransitions[arc.transitionId], arc.fromPlace, arc.channels, arc.isInformationLink);
         petriNet.arcs[i] = restoreArc(petriArc, arc);
     }
     return petriNet;
